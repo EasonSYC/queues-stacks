f19191f2498b9ca19f834a55163675e10bb7dfdf
Eason Shao <eason.syc@icloud.com>
2023-12-08 15:13:32 +0000

    implement dynamic queue & stack using nodes & links

---
+++ QueuesStacks.Classes.Implementation/Node.cs
@@ -0,0 +1,17 @@
+﻿namespace QueuesStacks.Classes.Implementation
+{
+    public class Node<T>
+    {
+        public T Value { get; private set; }
+        public Node<T>? Next { get; set; } // this acts as a link to the next node
+        public Node<T>? Prev { get; set; } // this acts as a link to the previous node
+
+        public Node(T value)
+        {
+            Value = value;
+            Next = null;
+            Prev = null;
+        }
+    }
+}
+
